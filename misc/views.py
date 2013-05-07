e60d3748a70d1de1af01959fd71009f23daebafb
Marcin Mincer <marcin.mincer@bladepolska.com>
2013-05-07 17:29:08 +0200

    make list not war

--- misc/views.py
+++ misc/views.py
@@ -10,28 +10,28 @@ from tuitter.models import Tuit
 
 def index(request):
     page = int(request.GET.get('page', 1))
-    recent_tuits = Tuit.objects.select_related('user').all().order_by("-added")[(page-1)*settings.TUITS_PER_PAGE:page*settings.TUITS_PER_PAGE]
+    recent_tuits = list(Tuit.objects.select_related('user').all().order_by("-added")[(page-1)*settings.TUITS_PER_PAGE:page*settings.TUITS_PER_PAGE])
 
     return render_to_response("index.html", {"recent_tuits": recent_tuits}, context_instance=RequestContext(request))
 
 
 def index_jinja2(request):
     page = int(request.GET.get('page', 1))
-    recent_tuits = Tuit.objects.select_related('user').all().order_by("-added")[(page-1)*settings.TUITS_PER_PAGE:page*settings.TUITS_PER_PAGE]
+    recent_tuits = list(Tuit.objects.select_related('user').all().order_by("-added")[(page-1)*settings.TUITS_PER_PAGE:page*settings.TUITS_PER_PAGE])
 
     return jinja_render_to_response("index_jinja2.html", {"recent_tuits": recent_tuits}, context_instance=RequestContext(request))
 
 
 def index_wo_user(request):
     page = int(request.GET.get('page', 1))
-    recent_tuits = Tuit.objects.select_related('user').all().order_by("-added")[(page-1)*settings.TUITS_PER_PAGE:page*settings.TUITS_PER_PAGE]
+    recent_tuits = list(Tuit.objects.select_related('user').all().order_by("-added")[(page-1)*settings.TUITS_PER_PAGE:page*settings.TUITS_PER_PAGE])
 
     return render_to_response("index_wo_user.html", {"recent_tuits": recent_tuits}, context_instance=RequestContext(request))
 
 
 def index_wo_user_jinja2(request):
     page = int(request.GET.get('page', 1))
-    recent_tuits = Tuit.objects.select_related('user').all().order_by("-added")[(page-1)*settings.TUITS_PER_PAGE:page*settings.TUITS_PER_PAGE]
+    recent_tuits = list(Tuit.objects.select_related('user').all().order_by("-added")[(page-1)*settings.TUITS_PER_PAGE:page*settings.TUITS_PER_PAGE])
 
     return jinja_render_to_response("index_wo_user_jinja2.html", {"recent_tuits": recent_tuits}, context_instance=RequestContext(request))
 
